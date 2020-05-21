abb6e02f498d1407f8ab54a38c7dda6fdb00ae02
tammi <tammichng.co@gmail.com>
2020-05-21 17:56:11 +0800

    implemented search function

--- script.js
+++ script.js
@@ -26,3 +26,32 @@ $("#finance-btn").click(function() {
     $(".card:not(.finance)").toggleClass("hide");
     $("#finance-btn").toggleClass("active-filter");
 })
+
+
+// FOR SEARCH FUNCTION
+
+function search() {
+
+    $(".hide").removeClass("hide");
+
+    var input, filter, cardtitle, cardtext;
+    input = $("#search").val();
+    filter = input.toUpperCase();
+    card = $(".card");
+    cardtitle = $(".card-title");
+    cardtext = $(".card-text");
+    
+    for (i=0; i < cardtitle.length; i++) {
+        currentCardTitle = cardtitle[i].innerText;
+        currentCardText = cardtext[i].innerText;
+        currentCard = card[i];
+
+        if (currentCardText.toUpperCase().indexOf(filter) > -1 || currentCardTitle.toUpperCase().indexOf(filter) > -1) {
+            console.log(currentCardText.toUpperCase(), filter, currentCardText.toUpperCase().indexOf(filter));
+        } else {
+            card[i].classList.add("hide");
+        }
+
+    }
+
+}
